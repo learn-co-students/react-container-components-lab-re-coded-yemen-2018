f21c787d8e6d0c30d8e73e5bab53b0ed49abf5f9
Farida <faridah.absi@gmail.com>
2018-11-03 18:53:43 +0000

    Done.

--- src/components/LatestMovieReviewsContainer.js
+++ src/components/LatestMovieReviewsContainer.js
@@ -1,8 +1,36 @@
 import React, { Component } from 'react';
+import MovieReviews from './MovieReviews';
 import 'isomorphic-fetch';
 
 const NYT_API_KEY = 'f98593a095b44546bf4073744b540da0';
-const URL = 'https://api.nytimes.com/svc/movies/v2/reviews/all.json?'
-            + `api-key=${NYT_API_KEY}`;
+const URL = 'https://api.nytimes.com/svc/movies/v2/reviews/all.json?';
 
 // Code LatestMovieReviewsContainer Here
+class LatestMovieReviewsContainer extends React.Component {
+  constructor(props) {
+    super(props);
+     this.state = {
+      reviews: []
+    };
+  }
+   fetchReviews = () => {
+    fetch(URL)
+    .then(response => response.json())
+    .then(reviews => {
+      this.setState({reviews: reviews.response})
+    })
+  };
+   componentDidMount(){
+    this.fetchReviews();
+  }
+
+   render() {
+    return (
+        <div className="latest-movie-reviews">
+          <h1>Latest reviews</h1>
+          <MovieReviews reviews={this.state.reviews}/>
+        </div>
+    );
+  }
+}
+ export default LatestMovieReviewsContainer;
--- src/components/MovieReviews.js
+++ src/components/MovieReviews.js
@@ -1,1 +1,34 @@
 // Code MovieReviews Here
+import React from 'react';
+
+ const Review = ({ byHow, headline, short_preview}) => {
+    return (
+        <div className="review">
+            <h3>{headline}</h3>
+            <h4>By:{byHow}</h4>
+            <p>{short_preview}</p>
+        </div>
+    )
+}
+ const MovieReviews = ({ reviews }) => {
+    return (
+        <div className="review-list">
+            <ul>
+            {reviews.map(review =>
+                <div className="review">
+                    <h3>{review.headline}</h3>
+                    <h4>By: {review.byHow}</h4>
+                    <p>{review.short_preview}</p>
+                </div>
+
+        )
+            }
+            </ul>
+        </div>
+    )
+};
+ MovieReviews.defaultProps = {
+    reviews: []
+}
+
+  export default MovieReviews;
