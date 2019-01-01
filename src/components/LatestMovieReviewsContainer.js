8b3b1e1039e04c84eb989f03bed2adc1da752375
Farida <faridah.absi@gmail.com>
2019-01-01 21:47:55 +0000

    Done.

--- src/components/LatestMovieReviewsContainer.js
+++ src/components/LatestMovieReviewsContainer.js
@@ -1,36 +1,35 @@
 import React, { Component } from 'react';
-import MovieReviews from './MovieReviews';
 import 'isomorphic-fetch';
+import MovieReviews from './MovieReviews';
 
 const NYT_API_KEY = 'f98593a095b44546bf4073744b540da0';
-const URL = 'https://api.nytimes.com/svc/movies/v2/reviews/all.json?';
+const URL = 'https://api.nytimes.com/svc/movies/v2/reviews/all.json?'
+            + `api-key=${NYT_API_KEY}`;
 
 // Code LatestMovieReviewsContainer Here
-class LatestMovieReviewsContainer extends React.Component {
-  constructor(props) {
-    super(props);
-     this.state = {
-      reviews: []
+class LatestMovieReviewsContainer extends Component {
+  constructor() {
+    super();
+    this.state = {
+      reviews: [ ]
     };
   }
-   fetchReviews = () => {
+
+  componentWillMount() {
     fetch(URL)
-    .then(response => response.json())
-    .then(reviews => {
-      this.setState({reviews: reviews.response})
-    })
-  };
-   componentDidMount(){
-    this.fetchReviews();
+      .then(res => res.json())
+      .then(response => this.setState({ reviews: response.results }));
   }
 
-   render() {
+  render() {
     return (
-        <div className="latest-movie-reviews">
-          <h1>Latest reviews</h1>
-          <MovieReviews reviews={this.state.reviews}/>
-        </div>
+      <div className="latest-movie-reviews">
+        <h1>Reviews:</h1>
+        <MovieReviews reviews={this.state.reviews} />
+      </div>
     );
   }
 }
- export default LatestMovieReviewsContainer;
+
+
+export default LatestMovieReviewsContainer;
--- src/components/MovieReviews.js
+++ src/components/MovieReviews.js
@@ -1,34 +1,30 @@
-// Code MovieReviews Here
+
 import React from 'react';
 
- const Review = ({ byHow, headline, short_preview}) => {
-    return (
-        <div className="review">
-            <h3>{headline}</h3>
-            <h4>By:{byHow}</h4>
-            <p>{short_preview}</p>
-        </div>
-    )
-}
- const MovieReviews = ({ reviews }) => {
-    return (
-        <div className="review-list">
-            <ul>
-            {reviews.map(review =>
-                <div className="review">
-                    <h3>{review.headline}</h3>
-                    <h4>By: {review.byHow}</h4>
-                    <p>{review.short_preview}</p>
-                </div>
+const Review = ({
+  headline,
+  byline,
+  link,
+  summary_short
+}) => {
+  return (
 
-        )
-            }
-            </ul>
-        </div>
-    )
+    <div key={headline} className="review">
+    <header>
+        <a className="review-link" href={link.url}>
+          {headline}
+        </a>
+        <span className="author">{byline}</span>
+    </header>
+      <p>{summary_short}</p>
+    </div>
+  );
 };
- MovieReviews.defaultProps = {
-    reviews: []
-}
 
-  export default MovieReviews;
+const MovieReviews = ({ reviews }) => <div className="review-list">{reviews.map(Review)}</div>;
+
+MovieReviews.defaultProps = {
+  reviews: []
+};
+
+export default MovieReviews;
